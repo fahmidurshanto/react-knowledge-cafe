c4f06eaaa9d0b721335c4125ec4f6eb1bf5f9ead
Fahmidur Rahaman Shanto <fahmidurrahamanshanto@gmail.com>
2023-11-02 21:46:51 +0600

    Added some elements to the blog.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Header></Header>
-      <main className="md: flex">
+      <main className="md: flex ">
         <Blogs></Blogs>
         <Bookmarks></Bookmarks>
       </main>
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,8 +1,32 @@
 import PropTypes from "prop-types";
 
 const Blog = ({ blog }) => {
-  console.log(blog);
-  return <div></div>;
+  const { title, cover, author, readingTime, authorImg, postedDate, hashtags } =
+    blog;
+
+  return (
+    <div>
+      <img src={cover} alt={`Cover picture of the title ${title}`} />
+      <h2 className="text-4xl">{title}</h2>
+      <p>{hashtags}</p>
+      <div className="flex justify-between">
+        <div className="flex ">
+          <img
+            className="w-24 rounded-full mr-6"
+            src={authorImg}
+            alt={` ${author} is the author of the content`}
+          />
+          <div>
+            <h1 className="text-2xl">{author}</h1>
+            <p>Posted Date: {postedDate}</p>
+          </div>
+        </div>
+        <div>
+          <p>Reading time {readingTime} min read</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 Blog.PropTypes = {
